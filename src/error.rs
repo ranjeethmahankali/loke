e512306364fbaede0bba2098e7f77720dad12245
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2026-09-13 21:48:12 -0400

    Display for errors

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Error {
     OutOfBounds(&'static str),
@@ -9,4 +11,22 @@ pub enum Error {
     ArcCannotBeCircle,
     DegenerateValue,
     InvalidArgument,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::OutOfBounds(msg) => write!(f, "Value out of bounds: {msg}"),
+            Error::InvalidParameter => write!(f, "Invalid parameter"),
+            Error::IncorrectKnotCount => write!(f, "Incorrect knot count"),
+            Error::InsufficientControlPoints => {
+                write!(f, "Insufficient control points to construct curve")
+            }
+            Error::RadiusTooSmall => write!(f, "Radius too small"),
+            Error::PointsCollinear => write!(f, "Points cannot be collinear"),
+            Error::ArcCannotBeCircle => write!(f, "Arc cannot be a full circle"),
+            Error::DegenerateValue => write!(f, "Value is degenerate"),
+            Error::InvalidArgument => write!(f, "Invalid argument"),
+        }
+    }
 }
